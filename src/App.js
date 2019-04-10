eba2516dfe7fa64646f79ac1717b678944045093
Sh4reef <shareef.banjar@gmail.com>
2019-04-11 02:17:35 +0800

    query topics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// apollo
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const TOPICS_QUERY = gql`
+  query getTopics {
+    topics(orderBy: votes_DESC) {
+      id
+      content
+      votes
+    }
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Query query={TOPICS_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Loading...</div>
+            if (error) return <div>Error!</div>
+            return data.topics.map((topic) => (
+              <div className="topic" key={topic.key}>
+                <div className="votes">
+                  <span className="upvote">▲</span>
+                  <span className="number">{topic.votes}</span>
+                  <span className="downvote">▼</span>
+                </div>
+                <p>
+                  {topic.content}
+                </p>
+              </div>
+            ))
+          }}
+        </Query>
       </div>
     );
   }
