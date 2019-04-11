f9a0b779ff151ab3a5ac9202043c8cc9a6448fa3
Sh4reef <shareef.banjar@gmail.com>
2019-04-11 21:20:48 +0800

    show loader when creating a topic

--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@ const Topic = withApollo(({ client, topic }) => {
 
 const Header = (props) => {
   const [show, setShow] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [content, setContent] = useState("")
   const invalid = content.length === 0 || content.length > 255
   const exceeded = content.length > 255
@@ -112,6 +113,7 @@ const Header = (props) => {
           <div className={show ? "modal show-modal" : "modal"}>
             <form onSubmit={async (e) => {
               e.preventDefault()
+              setLoading(!loading)
               await newTopic({
                 optimisticResponse: {
                   newTopic: {
@@ -121,12 +123,13 @@ const Header = (props) => {
                     __typename: "Topic"
                   }
                 },
-                update: (proxy, {data: newTopic}) => {
-                  const {topics} = proxy.readQuery({query: TOPICS_QUERY})
+                update: (proxy, { data: newTopic }) => {
+                  const { topics } = proxy.readQuery({ query: TOPICS_QUERY })
                   topics.push(newTopic.newTopic)
-                  proxy.writeQuery({query: TOPICS_QUERY, data: {topics}})
+                  proxy.writeQuery({ query: TOPICS_QUERY, data: { topics } })
                 }
               })
+              setLoading(!loading)
               setShow(!show)
             }}>
               <div className="label">
@@ -136,7 +139,15 @@ const Header = (props) => {
               <div className="number-of-characters" style={{ color: exceeded ? "red" : "" }}>
                 {255 - content.length}/255 {exceeded && "Exceeded"}
               </div>
-              <button type="submit" disabled={invalid} value={content} className="create-btn">Create</button>
+              <button
+                type="submit"
+                disabled={invalid || loading}
+                value={content}
+                className="create-btn"
+                style={{ paddingLeft: loading && "30px" }}>
+                {loading && <i className="loader"></i>}
+                Create
+              </button>
               <button onClick={(e) => {
                 e.preventDefault()
                 setShow(!show)
