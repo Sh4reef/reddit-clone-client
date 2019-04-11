9cf8b3341150615fdd8c34dd10950e3a35eb842c
Sh4reef <shareef.banjar@gmail.com>
2019-04-11 22:00:35 +0800

    move Topic component into a separate file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // apollo
 import gql from 'graphql-tag';
-import { Query, withApollo } from 'react-apollo';
+import { Query } from 'react-apollo';
 // components
 import Header from './components/Header';
+import Topic from './components/Topic';
 
 const TOPICS_QUERY = gql`
   query getTopics {
@@ -15,77 +16,6 @@ const TOPICS_QUERY = gql`
     }
   }
 `
-
-const UPVOTE_MUTATION = gql`
-  mutation UpVote($id: ID!) {
-    upVote(id: $id) {
-      id content votes
-    }
-  }
-`
-
-const DOWNVOTE_MUTATION = gql`
-  mutation DownVote($id: ID!) {
-    downVote(id: $id) {
-      id content votes
-    }
-  }
-`
-
-const Topic = withApollo(({ client, topic }) => {
-  const [loading, setLoading] = useState(false)
-  return (
-    <div className="topic" key={topic.key}>
-      <div className="votes">
-        <span onClick={(e) => {
-          setLoading(true)
-          !loading && client.mutate({
-            mutation: UPVOTE_MUTATION,
-            variables: { id: topic.id },
-            optimisticResponse: {
-              upVote: {
-                __typename: "Topic",
-                id: topic.id,
-                content: topic.content,
-                votes: ++topic.votes
-              }
-            },
-            update: (proxy, { data }) => {
-              const { topics } = proxy.readQuery({ query: TOPICS_QUERY })
-              const sortedTopics = topics.sort((a, b) => b.votes - a.votes)
-              proxy.writeQuery({ query: TOPICS_QUERY, data: { topics: sortedTopics } })
-            }
-          }).then(() => setLoading(false))
-        }}
-          className="upvote" style={{ cursor: loading && "progress", color: loading && "silver" }}>▲</span>
-        <span className="number">{topic.votes}</span>
-        <span onClick={(e) => {
-          setLoading(true)
-          !loading && client.mutate({
-            mutation: DOWNVOTE_MUTATION,
-            variables: { id: topic.id },
-            optimisticResponse: {
-              downVote: {
-                __typename: "Topic",
-                id: topic.id,
-                content: topic.content,
-                votes: --topic.votes
-              }
-            },
-            update: (proxy, { data }) => {
-              const { topics } = proxy.readQuery({ query: TOPICS_QUERY })
-              const sortedTopics = topics.sort((a, b) => b.votes - a.votes)
-              proxy.writeQuery({ query: TOPICS_QUERY, data: { topics: sortedTopics } })
-            }
-          }).then(() => setLoading(false))
-        }} className="downvote" style={{ cursor: loading && "progress", color: loading && "silver" }}>▼</span>
-      </div>
-      <p className="content">
-        {topic.content}
-      </p>
-    </div>
-  )
-})
 
 class App extends Component {
   render() {
---
+++ src/components/Topic/index.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+// apollo
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
+
+const TOPICS_QUERY = gql`
+  query getTopics {
+    topics(orderBy: votes_DESC) {
+      id
+      content
+      votes
+    }
+  }
+`
+
+const UPVOTE_MUTATION = gql`
+  mutation UpVote($id: ID!) {
+    upVote(id: $id) {
+      id content votes
+    }
+  }
+`
+
+const DOWNVOTE_MUTATION = gql`
+  mutation DownVote($id: ID!) {
+    downVote(id: $id) {
+      id content votes
+    }
+  }
+`
+
+export default withApollo(({ client, topic }) => {
+  const [loading, setLoading] = useState(false)
+  return (
+    <div className="topic" key={topic.key}>
+      <div className="votes">
+        <span onClick={(e) => {
+          setLoading(true)
+          !loading && client.mutate({
+            mutation: UPVOTE_MUTATION,
+            variables: { id: topic.id },
+            optimisticResponse: {
+              upVote: {
+                __typename: "Topic",
+                id: topic.id,
+                content: topic.content,
+                votes: ++topic.votes
+              }
+            },
+            update: (proxy, { data }) => {
+              const { topics } = proxy.readQuery({ query: TOPICS_QUERY })
+              const sortedTopics = topics.sort((a, b) => b.votes - a.votes)
+              proxy.writeQuery({ query: TOPICS_QUERY, data: { topics: sortedTopics } })
+            }
+          }).then(() => setLoading(false))
+        }}
+          className="upvote" style={{ cursor: loading && "progress", color: loading && "silver" }}>▲</span>
+        <span className="number">{topic.votes}</span>
+        <span onClick={(e) => {
+          setLoading(true)
+          !loading && client.mutate({
+            mutation: DOWNVOTE_MUTATION,
+            variables: { id: topic.id },
+            optimisticResponse: {
+              downVote: {
+                __typename: "Topic",
+                id: topic.id,
+                content: topic.content,
+                votes: --topic.votes
+              }
+            },
+            update: (proxy, { data }) => {
+              const { topics } = proxy.readQuery({ query: TOPICS_QUERY })
+              const sortedTopics = topics.sort((a, b) => b.votes - a.votes)
+              proxy.writeQuery({ query: TOPICS_QUERY, data: { topics: sortedTopics } })
+            }
+          }).then(() => setLoading(false))
+        }} className="downvote" style={{ cursor: loading && "progress", color: loading && "silver" }}>▼</span>
+      </div>
+      <p className="content">
+        {topic.content}
+      </p>
+    </div>
+  )
+})
